b3e462cc3f862f257c6ef54f459f6bbb5f2569c4
Trevor Mwangi <trevorzac16@yahoo.com>
2022-09-08 20:54:46 -0400

    add hhtp request to backend

--- src/client.js
+++ src/client.js
@@ -1,1 +1,10 @@
-import fetch from 'unfetch'
+import fetch from "unfetch";
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  }
+};
+export const getAllStudents = () => {
+  fetch("http://localhost:8080/api/v1/students").then(checkStatus);
+};
